e3e0592869b0104c18e3edf795a5d8ee30901334
xxhong <xxhong@buaa.edu.cn>
2025-12-25 11:37:03 +0800

    first commit

---
+++ build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/vcpkg-dependencies/toolchain.cmake
@@ -0,0 +1,8 @@
+
+        set(CMAKE_C_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe")
+        set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.44.35207/bin/HostX64/x64/cl.exe")
+        include("C:/Users/xxhong/jobs/tools/vcpkg/scripts/buildsystems/vcpkg.cmake")
+
+        set(VCPKG_TARGET_TRIPLET x64-windows-static)
+        include("C:/Users/xxhong/jobs/tools/vcpkg/scripts/buildsystems/vcpkg.cmake")
+      
